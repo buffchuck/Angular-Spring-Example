fdc15f735ae5a4f91b44a6cd83e5b6a6ce7339ef
rarnold <rarnold@lb.com>
2019-07-22 22:30:05 -0400

    Update UserRepository.java
    Rid of unused import

--- src/main/java/io.com.arnold.robert/application/repositories/UserRepository.java
+++ src/main/java/io.com.arnold.robert/application/repositories/UserRepository.java
@@ -3,7 +3,6 @@ package io.com.arnold.robert.application.repositories;
 import io.com.arnold.robert.application.entities.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long>{}
